ee4d47e41c6bd2b9d3afe17885fceea879b2fc9f
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-06-14 00:30:49 -0600

    refactor: add permanent redirect on not found page

--- app/[lang]/[[...slug]]/page.tsx
+++ app/[lang]/[[...slug]]/page.tsx
@@ -1,7 +1,7 @@
 import { getLanguages, getPage, getPages } from "@/app/source";
 import type { Metadata } from "next";
 import { DocsPage, DocsBody } from "fumadocs-ui/page";
-import { notFound } from "next/navigation";
+import { notFound, permanentRedirect, redirect } from "next/navigation";
 
 export default async function Page({
 	params,
@@ -42,7 +42,9 @@ export function generateMetadata({
 }) {
 	const page = getPage(params.slug, params.lang);
 
-	if (page == null) notFound();
+	if (page == null) {
+		permanentRedirect("/get-started/introduction");
+	}
 
 	return {
 		title: page.data.title,
