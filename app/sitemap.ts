c5a616a2acd385ded2bedeb1b13c98b4078bd1a9
Mauricio Siu <47042324+Siumauricio@users.noreply.github.com>
2024-07-03 23:51:12 -0600

    refactor: update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -7,12 +7,6 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
 		...getPages().map<MetadataRoute.Sitemap[number]>((page) => {
 			return {
 				url: url(`/en${page.url}`),
-				alternates: {
-					languages: {
-						zh: `${url("/")}cn${page.url}`,
-						en: `${url("/")}en${page.url}`,
-					},
-				},
 				lastModified: page.data.exports.lastModified
 					? new Date(page.data.exports.lastModified)
 					: undefined,
